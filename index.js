57ca6fb78ba7683420f91e8fecfc5bd22387d729
m49774686-hash <m49774686@gmail.com>
2026-07-14 13:00:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,68 +2,9 @@ require("dotenv").config();
 
 const TelegramBot = require("node-telegram-bot-api");
 
-
 const bot = new TelegramBot(
   process.env.BOT_TOKEN,
   {
     polling: true
   }
 );
-
-
-const ADMIN_ID = "8854496824";
-
-
-// Start message
-bot.onText(/\/start/, (msg) => {
-
-  bot.sendMessage(
-    msg.chat.id,
-`👋 Welcome to CineXClub Admin Bot
-
-Please tell us what help you need.
-
-📩 Send your message here and our admin will reply soon.`
-  );
-
-});
-
-
-// User messages
-bot.on("message", async (msg) => {
-
-
-  if (msg.text && msg.text.startsWith("/")) {
-    return;
-  }
-
-
-  if (msg.from.id.toString() === ADMIN_ID) {
-    return;
-  }
-
-
-  await bot.sendMessage(
-    ADMIN_ID,
-`📩 New User Message
-
-👤 User ID:
-${msg.from.id}
-
-💬 Message:
-${msg.text || "Media/File"}`
-  );
-
-
-});
-
-
-bot.on("polling_error", (err)=>{
-  console.log(
-    "Error:",
-    err.message
-  );
-});
-
-
-console.log("🤖 Admin Bot Started");
